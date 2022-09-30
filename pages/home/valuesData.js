6da27e3eedb74315f6d0d019b1448a9506d68ede
Rafael Jaochitz Bertoldo <89934748+rafael-bertoldo@users.noreply.github.com>
2022-09-30 17:33:38 -0300

    Update valuesData.js

--- pages/home/valuesData.js
+++ pages/home/valuesData.js
@@ -4,16 +4,16 @@ let insertedValues = [
   {
     id: 1,
     value: 90.0,
-    categoryID: 1,
+    categoryID: 0,
   },
   {
     id: 2,
     value: 40.0,
-    categoryID: 2,
+    categoryID: 1,
   },
   {
     id: 3,
     value: 15.5,
-    categoryID: 1,
+    categoryID: 0,
   },
 ];
